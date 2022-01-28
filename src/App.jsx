ff6b37dffe4666ae92d3e8ac7699961843321bd1
BoikoVD <boiko.volodymyr.dm@gmail.com>
2022-01-28 07:47:41 +0200

    Delete comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,7 @@ import ListHeader from './components/ListHeader/ListHeader';
 import TaskList from './components/TaskList/TaskList';
 
 const App = () => {
-	/*
-		componentDidUpdate(prevProps, prevState) {
-			if (this.state.tasks.length > prevState.tasks.length) {
-				const addedTask = this.state.tasks.find(newStateEl => {
-					return prevState.tasks.findIndex(oldStateEl => JSON.stringify(oldStateEl) === JSON.stringify(newStateEl)) === -1
-				});
-				console.log(`Task "${addedTask.text}" added`);
-			}
-			localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
-		}
-	*/
+
 	return (
 		<div className={cl.wrapper}>
 			<div className={cl.body}>
